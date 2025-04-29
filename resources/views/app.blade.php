ac318f29e5509b0daf21fc9f3d796a6594957303
Elisha Winner <elisha.offically@gmail.com>
2025-04-29 14:04:05 +0100

    Add action

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,12 +6,14 @@
 
         <title inertia>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
+
+
+        
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
 
-        <!-- Scripts -->
+
         @routes
         @viteReactRefresh
         @vite(['resources/js/app.tsx', "resources/js/Pages/{$page['component']}.tsx"])
